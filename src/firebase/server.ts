bf4f0e2355c94b2ac9081948e1df8a950fb6e15a
tranquangdai <dai.tran@cloudteam.vn>
2025-10-07 15:17:18 +0700

    expose cridentials on CI CD

--- src/firebase/server.ts
+++ src/firebase/server.ts
@@ -1,13 +1,31 @@
-import admin from 'firebase-admin'
-import serviceAccountKey from './serviceAccountKey.json'
+import admin, { ServiceAccount } from 'firebase-admin'
+import { readFileSync } from 'fs';
 
 let app: admin.app.App
 
 if (!admin.apps.length) {
-    app =    admin.initializeApp({
-        credential: admin.credential.cert(serviceAccountKey as admin.ServiceAccount)
+
+    const credentialsPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
+
+    let serviceAccount: ServiceAccount;
+
+    if (!credentialsPath) {
+        const credentialsJsonEncoded =
+            process.env.GOOGLE_APPLICATION_CREDENTIALS_BASE64;
+        const credentialsJson = credentialsJsonEncoded
+            ? Buffer.from(credentialsJsonEncoded, "base64").toString("utf-8")
+            : "{}";
+        serviceAccount = JSON.parse(credentialsJson) as ServiceAccount;
+    } else {
+        // serviceAccount = require(credentialsPath) as ServiceAccount;
+        const credentialsJson = readFileSync(credentialsPath, "utf-8");
+        serviceAccount = JSON.parse(credentialsJson) as ServiceAccount;
+    }
+
+    app = admin.initializeApp({
+        credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
     })
-}else{
+} else {
     app = admin.app()
 }
 
